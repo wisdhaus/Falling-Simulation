e267aa389e051c6c48f488964b183289fdceddcf
wisdhaus <wisdhaus@gmail.com>
2024-11-30 00:52:34 +0300

    Structures

--- Falling Simulation/Program.cs
+++ Falling Simulation/Program.cs
@@ -1,24 +1,26 @@
 ﻿Console.ForegroundColor = ConsoleColor.Cyan;
+
 int acceleration_of_gravity = -10;
 int hight = 30;
 int initial_y = 30;
 double y_position = initial_y;
-int mass = 1;
 const double DELTA_T = 0.05;
 int initial_speed = 0;
-double speed = initial_speed;
+
 double e_kinetic = 0;
 double e_potential = 0;
 double losses = 0.8;
 double full_energy = 0;
 double scale = 1;
 double scaled_y;
+
 string[] Array = new string[Convert.ToInt16(Math.Round(scale * hight, 0)) + 1];
 string block = "\u2588";
 string upper_half_of_block = "\u2580";
 string lower_half_of_block = "\u2584";
-double additional_losses = 0.1 * mass;
+
 int timer = -1;
+
 void Enter()
 {
     while (Console.ReadKey().Key != ConsoleKey.Enter) { }
@@ -26,7 +28,7 @@ void Enter()
 void Data()
 {
     Console.WriteLine(
-        $"Speed: {Math.Round(speed, 1)}\n"
+        $""
             + $"Hight: {Math.Round(y_position, 1)}\n"
             + $"E_kinetic: {Math.Round(e_kinetic, 1)}\n"
             + $"E_potential: {Math.Round(e_potential, 1)}\n"
@@ -41,34 +43,36 @@ void Update()
     Thread.Sleep(5);
     Console.Clear();
 }
-void Physics(double delta_time)
+Object Physics(double delta_time, Object ball)
 {
-    speed += acceleration_of_gravity * delta_time / 2;
-    y_position += speed * delta_time;
-    if (y_position <= 0 && speed < 0)
+    double additional_losses = 0.1 * ball.Mass;
+    ball.Speed += acceleration_of_gravity * delta_time / 2;
+    y_position += ball.Speed * delta_time;
+    if (y_position <= 0 && ball.Speed < 0)
     {
         e_kinetic = e_kinetic * losses - additional_losses;
         if (e_kinetic < 0)
         {
             e_kinetic = 0;
         }
-        speed = Math.Sqrt(2 * e_kinetic / mass);
+        ball.Speed = Math.Sqrt(2 * e_kinetic / ball.Mass);
         y_position = 0;
     }
-    if (y_position >= hight && speed > 0)
+    if (y_position >= hight && ball.Speed > 0)
     {
         e_kinetic = e_kinetic * losses - additional_losses;
         if (e_kinetic < 0)
         {
             e_kinetic = 0;
         }
-        speed = -Math.Sqrt(2 * e_kinetic / mass);
+        ball.Speed = -Math.Sqrt(2 * e_kinetic / ball.Mass);
         y_position = hight;
     }
-    e_kinetic = mass * Math.Pow(speed, 2) / 2;
-    e_potential = -(mass * acceleration_of_gravity * y_position);
+    e_kinetic = ball.Mass * Math.Pow(ball.Speed, 2) / 2;
+    e_potential = -(ball.Mass * acceleration_of_gravity * y_position);
     full_energy = e_kinetic + e_potential;
     timer++;
+    return ball;
 }
 void Graphics()
 {
@@ -95,14 +99,21 @@ void Graphics()
     }
 }
 Enter();
+var ball = new Object { Mass = 1, Speed = 0 };
 do
 {
     Update();
     for (int i = 0; i < 5; i++)
     {
-        Physics(DELTA_T / 5);
+        ball = Physics(DELTA_T / 5, ball);
     }
     Data();
     Graphics();
 } while (full_energy > 0);
 Enter();
+
+struct Object
+{
+    public int Mass;
+    public double Speed;
+}
