db331aecafdcee28c3f0d7a00c999ae19c2f095e
wisdhaus <wisdhaus@gmail.com>
2024-11-30 19:45:00 +0300

    Added accuracy

--- Falling Simulation/Program.cs
+++ Falling Simulation/Program.cs
@@ -1,126 +1,153 @@
 ﻿Console.ForegroundColor = ConsoleColor.Cyan;
 
-int acceleration_of_gravity = -10;
-int hight = 30;
-int initial_y = 30;
+int hight;
+Console.Write("Enter hight: ");
+hight = Convert.ToInt16(Console.ReadLine());
 
-const double DELTA_T = 0.05;
-int initial_speed = 0;
+int initial_y;
+Console.Write("Enter initial Y position: ");
+initial_y = Convert.ToInt16(Console.ReadLine());
 
-double e_kinetic = 0;
-double e_potential = 0;
-double losses = 0.8;
-double full_energy = 0;
-double scale = 1;
-double scaled_y;
+double scale;
+Console.Write("Enter scale: ");
+scale = Convert.ToDouble(Console.ReadLine());
 
-string[] Array = new string[Convert.ToInt16(Math.Round(scale * hight, 0)) + 1];
-string block = "\u2588";
-string upper_half_of_block = "\u2580";
-string lower_half_of_block = "\u2584";
+double losses;
+Console.Write("Enter losses: ");
+losses = Convert.ToDouble(Console.ReadLine());
 
-int timer = -1;
+int initial_speed;
+Console.Write("Enter initial speed: ");
+initial_speed = Convert.ToInt16(Console.ReadLine());
 
-void Enter()
-{
-    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-}
-void Data(Object ball)
-{
-    Console.WriteLine(
-        $"Speed: {Math.Round(ball.Speed, 1)}\n"
-            + $"Hight: {Math.Round(ball.Y, 1)}\n"
-            + $"E_kinetic: {Math.Round(e_kinetic, 1)}\n"
-            + $"E_potential: {Math.Round(e_potential, 1)}\n"
-            + $"Initial Hight: {initial_y}\n"
-            + $"Initial Speed: {initial_speed}\n"
-            + $"Scale: {scale} lines/meters\n"
-            + $"Timer: {timer}"
-    );
-}
+int mass;
+Console.Write("Enter mass: ");
+mass = Convert.ToInt16(Console.ReadLine());
+
+double acceleration_of_gravity;
+Console.Write("Enter acceleration of gravity: ");
+acceleration_of_gravity = Convert.ToDouble(Console.ReadLine());
+
+double accuracy;
+Console.Write("Enter accuracy: ");
+accuracy = Convert.ToInt16(Console.ReadLine());
+
 void Update()
 {
     Thread.Sleep(5);
     Console.Clear();
 }
-Object Physics(double delta_time, Object ball)
+
+double timer = -1;
+
+Physical_characteristics Physics(double delta_time, Physical_characteristics block)
 {
-    double additional_losses = 0.1 * ball.Mass;
-    ball.Speed += acceleration_of_gravity * delta_time / 2;
-    ball.Y += ball.Speed * delta_time;
-    if (ball.Y <= 0 && ball.Speed < 0)
+    block.Additional_losses = 0.1 * block.Mass;
+    block.Speed += acceleration_of_gravity * delta_time / 2;
+    block.Y += block.Speed * delta_time;
+    if (block.Y <= 0 && block.Speed < 0)
     {
-        e_kinetic = e_kinetic * losses - additional_losses;
-        if (e_kinetic < 0)
+        block.E_kinetic = block.E_kinetic * block.Losses - block.Additional_losses;
+        if (block.E_kinetic < 0)
         {
-            e_kinetic = 0;
+            block.E_kinetic = 0;
         }
-        ball.Speed = Math.Sqrt(2 * e_kinetic / ball.Mass);
-        ball.Y = 0;
+        block.Speed = Math.Sqrt(2 * block.E_kinetic / block.Mass);
+        block.Y = 0;
     }
-    if (ball.Y >= hight && ball.Speed > 0)
+    if (block.Y >= hight && block.Speed > 0)
     {
-        e_kinetic = e_kinetic * losses - additional_losses;
-        if (e_kinetic < 0)
+        block.E_kinetic = block.E_kinetic * block.Losses - block.Additional_losses;
+        if (block.E_kinetic < 0)
         {
-            e_kinetic = 0;
+            block.E_kinetic = 0;
         }
-        ball.Speed = -Math.Sqrt(2 * e_kinetic / ball.Mass);
-        ball.Y = hight;
+        block.Speed = -Math.Sqrt(2 * block.E_kinetic / block.Mass);
+        block.Y = hight;
     }
-    e_kinetic = ball.Mass * Math.Pow(ball.Speed, 2) / 2;
-    e_potential = -(ball.Mass * acceleration_of_gravity * ball.Y);
-    full_energy = e_kinetic + e_potential;
-    timer++;
-    return ball;
+    block.E_kinetic = block.Mass * Math.Pow(block.Speed, 2) / 2;
+    block.E_potential = -(block.Mass * acceleration_of_gravity * block.Y);
+    block.Full_energy = block.E_kinetic + block.E_potential;
+    timer = timer + (1 / accuracy);
+    return block;
 }
-void Graphics(Object ball)
+
+void Data(Physical_characteristics block)
 {
-    scaled_y = scale * ball.Y;
-    for (int i = 0; i < Array.Length; i++)
+    Console.WriteLine(
+        $"Speed: {Math.Round(block.Speed, 1)}\n"
+            + $"Y_position: {Math.Round(block.Y, 1)}\n"
+            + $"E_kinetic: {Math.Round(block.E_kinetic, 1)}\n"
+            + $"E_potential: {Math.Round(block.E_potential, 1)}\n"
+            + $"Timer: {Math.Round(timer, 0)}"
+    );
+}
+
+string[] Lines = new string[(int)(Math.Round(scale * hight, 0)) + 1];
+const string BLOCK = "\u2588";
+const string UPPER_HALF_OF_BLOCK = "\u2580";
+const string LOWER_HALF_OF_BLOCK = "\u2584";
+double scaled_y;
+
+void Graphics(Physical_characteristics block)
+{
+    scaled_y = scale * block.Y;
+    for (int i = 0; i < Lines.Length; i++)
     {
-        Array[i] = $"{i}";
+        Lines[i] = $"{i}";
     }
-    if (scaled_y < Array.Length - 1)
+    if (scaled_y < Lines.Length - 1)
     {
         if (scaled_y - Math.Floor(scaled_y) >= 0 && scaled_y - Math.Floor(scaled_y) < 0.5)
         {
-            Array[Convert.ToInt32(Math.Round(scaled_y, 0))] = block;
+            Lines[(int)(Math.Round(scaled_y, 0))] = BLOCK;
         }
         if (scaled_y - Math.Floor(scaled_y) >= 0.5 && scaled_y - Math.Floor(scaled_y) < 1)
         {
-            Array[Convert.ToInt32(Math.Floor(scaled_y))] = upper_half_of_block;
-            Array[Convert.ToInt32(Math.Round(scaled_y, 0))] = lower_half_of_block;
+            Lines[(int)(Math.Floor(scaled_y))] = UPPER_HALF_OF_BLOCK;
+            Lines[(int)(Math.Round(scaled_y, 0))] = LOWER_HALF_OF_BLOCK;
         }
     }
-    for (int i = 0; i < Array.Length; i++)
+    for (int i = 0; i < Lines.Length; i++)
     {
-        Console.WriteLine(Array[Array.Length - i - 1]);
+        Console.WriteLine(Lines[Lines.Length - i - 1]);
     }
 }
 
-var ball = new Object
+var block = new Physical_characteristics
 {
     Mass = 1,
     Speed = initial_speed,
-    Y = initial_y
+    Y = initial_y,
+    Losses = losses,
+    Additional_losses = 0,
+    E_kinetic = 0,
+    E_potential = 0,
+    Full_energy = 0,
 };
-Enter();
+
+const double DELTA_T = 0.05;
+
 do
 {
     Update();
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < accuracy; i++)
     {
-        ball = Physics(DELTA_T / 5, ball);
+        block = Physics(DELTA_T / accuracy, block);
     }
-    Data(ball);
-    Graphics(ball);
-} while (full_energy > 0);
-Enter();
+    Data(block);
+    Graphics(block);
+} while (block.Full_energy > 0);
+while (Console.ReadKey().Key != ConsoleKey.Enter) { }
 
-struct Object
+struct Physical_characteristics
 {
     public int Mass;
     public double Speed;
     public double Y;
+    public double Losses;
+    public double E_kinetic;
+    public double E_potential;
+    public double Full_energy;
+    public double Additional_losses;
 }
